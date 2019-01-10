68ba9518216bd9dfe93be095375560057a59ba8a
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2019-01-10 10:55:37 +0100

    Main menu

---
+++ Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public void Rotater(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+
+
+}
